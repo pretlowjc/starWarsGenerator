f3fd41307f27eee931e3e04874705301b14e7018
pretlowjc <justin.pretlow13@gmail.com>
2025-07-02 20:58:18 -0400

    Getting started on initial setup of directory and started adding to quotes object.

---
+++ quotes.js
@@ -0,0 +1,109 @@
+// Star Wars Quotes Generator
+// This file contains a collection of quotes from various Star Wars characters.
+// The quotes are categorized by character, movie, and trilogy.
+/*
+
+	Blueprint of a quote object:
+	{
+		character: ,
+		quote: ,
+		movie: ,
+		trilogy:
+	},
+
+	Use to speed up the process of adding new quotes.
+
+*/
+
+const quotes = [
+	{
+		character: "Darth Vader",
+		quote: "I've been waiting for you, Obi-Wan. We meet again, at last. The circle is now complete. When I left you, I was but the learner; now I am the master.",
+		movie: "A New Hope",
+		trilogy: "Original"
+	},
+	{
+		character: "Darth Vader",
+		quote: "The Force is strong with this one.",
+		movie: "A New Hope",
+		trilogy: "Original"
+	},
+	{
+		character: "Darth Vader",
+		quote: "No, I am your father.",
+		movie: "The Empire Strikes Back",
+		trilogy: "Original"
+	},
+	{
+		character: "Darth Vader",
+		quote: "I find you lack of faith disturbing.",
+		movie: "A New Hope",
+		trilogy: "Original"
+	},
+	{
+		character: "Darth Vader",
+		quote: "If you only knew the power of the dark side.",
+		movie: "The Empire Strikes Back",
+		trilogy: "Original"
+	},
+	{
+		character: "Darth Vader",
+		quote: "Be careful not to choke on your aspirations.",
+		movie: "Rogue One",
+		trilogy: "Prequel"
+	},
+	{
+		character: "Darth Vader",
+		quote: "You have failed me for the last time.",
+		movie: "The Empire Strikes Back",
+		trilogy: "Original"
+	},
+	{
+		character: "Darth Vader",
+		quote: "You have controlled your fear. Now, release your anger. Only your hatred can destroy me.",
+		movie: "The Empire Strikes Back",
+		trilogy: "Original"
+	},
+	{
+		character: "Yoda",
+		quote: "Do, or do not. There is no try.",
+		movie: "The Empire Strikes Back",
+		trilogy: "Original"
+	},
+	{
+		character: "Obi-Wan Kenobi",
+		quote: "Remember...the Force will be with you, always.",
+		movie: "A New Hope",
+		trilogy: "Original"
+	},
+	{
+		character: "Obi-Wan Kenobi",
+		quote: "Use the Force, Luke.",
+		movie: "A New Hope",
+		trilogy: "Original"
+	},
+	{
+		character: "Obi-Wan Kenobi",
+		quote: "You were the chosen one! It was said that you would destroy the Sith, not join them! Bring balance to the Force, not leave it in darkness!",
+		movie: "Revenge of the Sith",
+		trilogy: "Prequel"
+	},
+	{
+		character: "Obi-Wan Kenobi",
+		quote: "Be mindful of your thoughts, Anakin. They'll betray you.",
+		movie: "Attack of the Clones",
+		trilogy: "Prequel"
+	},
+	{
+		character: "Obi-Wan Kenobi",
+		quote: "In my experience, there is no such thing as luck.",
+		movie: "A New Hope",
+		trilogy: "Original"
+	},
+	{
+		character: "Obi-Wan Kenobi",
+		quote: "Your eyes can deceive you, don't trust them.",
+		movie: "A New Hope",
+		trilogy: "Original"
+	}
+];
