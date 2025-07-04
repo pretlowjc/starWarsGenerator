057a378dd12442f72e0a9be26eaacc55edab05e9
pretlowjc <justin.pretlow13@gmail.com>
2025-07-03 20:46:11 -0400

    Successfully generates random quotes from quote object. Prints to console with basic format.

--- generator.js
+++ generator.js
@@ -1,4 +1,16 @@
 const quotes = require('./quotes');
 
+function getRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+}
+
+function displayQuote() {
+    const quote = getRandomQuote();
+    console.log(`"${quote.quote}" \n - ${quote.character} \n Movie: ${quote.movie} \n Trilogy: ${quote.trilogy}`);
+}
+
+displayQuote();
+
 
 
